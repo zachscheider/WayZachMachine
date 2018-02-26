86bf4129f2758564d4cc2a6a840a98cd695a2d93
Zach Scheider <zachscheider@gmail.com>
2018-02-26 13:56:00 -0500

    Fetch page using wget

---
+++ archive.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from subprocess import run, PIPE, DEVNULL
+
+import config
+
+# URL helpers
+without_scheme = lambda url: url.replace('http://', '').replace('https://', '').replace('ftp://', '')
+without_query = lambda url: url.split('?', 1)[0]
+without_hash = lambda url: url.split('#', 1)[0]
+without_path = lambda url: url.split('/', 1)[0]
+file_name = lambda url: url.split('/')[-1]
+without_extension = lambda file_name: file_name.split('.')[0]
+domain = lambda url: without_hash(without_query(without_path(without_scheme(url))))
+base_url = lambda url: without_query(without_scheme(url))
+
+def grab_link(path):
+  # Read first line of file
+  link = ""
+  with open(link_file, 'r') as f:
+    link = f.readline().strip()
+
+  info = {
+      'url': link,
+      'domain': domain(link),
+      'base_url': base_url(link),
+      'title': base_url(link),
+      'file_name': file_name(link),
+      'page': without_extension(file_name(link))
+  }
+  return info
+
+def fetch_wget(link, timeout=config.TIMEOUT):
+  CMD = [
+    *'wget -E -H -k -p -e robots=off'.split(' '),
+    link['url'],
+  ]
+
+  try:
+    result = run(CMD, stdout=PIPE, stderr=PIPE, cwd=config.ARCHIVE_DIR, timeout=timeout + 1)  # index.html
+    if result.returncode > 0:
+      print('       got wget response code {}:'.format(result.returncode))
+      print('\n'.join('         ' + line for line in (result.stderr or result.stdout).decode().rsplit('\n', 10)[-10:] if line.strip()))
+      # raise Exception('Failed to wget download')
+  except Exception as e:
+    print('       Run to see full output:', 'cd {}; {}'.format(config.ARCHIVE_DIR, ' '.join(CMD)))
+
+if __name__ == "__main__":
+  argc = len(sys.argv)
+
+  if argc < 2 or set(sys.argv).intersection(['-h', '--help']):
+    print("Usage:")
+    print("    python archive.py ~/Downloads/bookmarks_export.txt\n")
+    raise SystemExit(0)
+
+  link_file = sys.argv[1]
+
+  if not os.path.exists(config.ARCHIVE_DIR):
+    os.makedirs(config.ARCHIVE_DIR)
+
+  link_info = grab_link(link_file)
+  fetch_wget(link_info)
---
+++ config.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import shutil
+
+from subprocess import run, PIPE
+
+# ******************************************************************************
+# * TO SET YOUR CONFIGURATION, EDIT THE VALUES BELOW, or use the 'env' command *
+# * e.g.                                                                       *
+# * env USE_COLOR=True CHROME_BINARY=google-chrome ./archive.py export.html    *
+# ******************************************************************************
+
+IS_TTY = sys.stdout.isatty()
+USE_COLOR =              os.getenv('USE_COLOR',              str(IS_TTY)        ).lower() == 'true'
+SHOW_PROGRESS =          os.getenv('SHOW_PROGRESS',          str(IS_TTY)        ).lower() == 'true'
+FETCH_WGET =             os.getenv('FETCH_WGET',             'True'             ).lower() == 'true'
+FETCH_WGET_REQUISITES =  os.getenv('FETCH_WGET_REQUISITES',  'True'             ).lower() == 'true'
+FETCH_AUDIO =            os.getenv('FETCH_AUDIO',            'False'            ).lower() == 'true'
+FETCH_VIDEO =            os.getenv('FETCH_VIDEO',            'False'            ).lower() == 'true'
+FETCH_PDF =              os.getenv('FETCH_PDF',              'True'             ).lower() == 'true'
+FETCH_SCREENSHOT =       os.getenv('FETCH_SCREENSHOT',       'True'             ).lower() == 'true'
+FETCH_FAVICON =          os.getenv('FETCH_FAVICON',          'True'             ).lower() == 'true'
+SUBMIT_ARCHIVE_DOT_ORG = os.getenv('SUBMIT_ARCHIVE_DOT_ORG', 'True'             ).lower() == 'true'
+RESOLUTION =             os.getenv('RESOLUTION',             '1920,1080'         )
+ARCHIVE_PERMISSIONS =    os.getenv('ARCHIVE_PERMISSIONS',    '755'              )
+ARCHIVE_DIR =            os.getenv('ARCHIVE_DIR',            '/home/zach/WayZachMachine')
+CHROME_BINARY =          os.getenv('CHROME_BINARY',          'chromium-browser' )  # change to google-chrome browser if using google-chrome
+WGET_BINARY =            os.getenv('WGET_BINARY',            'wget'             )
+WGET_USER_AGENT =        os.getenv('WGET_USER_AGENT',         None)
+CHROME_USER_DATA_DIR =   os.getenv('CHROME_USER_DATA_DIR',    None)
+TIMEOUT =                int(os.getenv('TIMEOUT',            '60'))
+LINK_INDEX_TEMPLATE =    os.getenv('LINK_INDEX_TEMPLATE',    'templates/link_index_fancy.html')
+INDEX_TEMPLATE =         os.getenv('INDEX_TEMPLATE',         'templates/index.html')
+INDEX_ROW_TEMPLATE =     os.getenv('INDEX_ROW_TEMPLATE',     'templates/index_row.html')
+
+### Output Paths
+ROOT_FOLDER = os.path.dirname(os.path.abspath(__file__))
+HTML_FOLDER = os.path.join(ARCHIVE_DIR, 'html')
+ARCHIVE_FOLDER = os.path.join(HTML_FOLDER, 'archive')
+os.chdir(ROOT_FOLDER)
